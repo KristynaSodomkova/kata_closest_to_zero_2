6a75956e85bdc398999e88b903ce67af0e81dcd5
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-19 20:12:45 +0100

    test for not counting twice passes, test for picking the words with the highest number of same letters fails

--- tests/test_zero.py
+++ tests/test_zero.py
@@ -6,3 +6,12 @@ list_of_str_for_test = ["post", "most", "frost", "does", "blast", "window"]
 def test_how_many_same_letters():
     assert how_many_same_letters("most", "post") == 3
 
+
+def test_does_not_count_twice():
+    assert how_many_same_letters("most", "mm") == 1
+
+def test_pick_the_highest_same_letters():
+    assert pick_the_highest_same_letters(list_of_str_for_test, "stop") == "post"
+
+#def test_find_the_shortest():
+ #   assert find_the_shortest() ==
