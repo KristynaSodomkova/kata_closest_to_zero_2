cc9f842998f2435eb0bb55432334632c2fb2ead4
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-19 19:58:04 +0100

    test for count of identical letters fails

--- tests/test_zero.py
+++ tests/test_zero.py
@@ -3,6 +3,5 @@ from kata_closest_to_zero_2.zero import *
 list_of_str_for_test = ["post", "most", "frost", "does", "blast", "window"]
 
 
-def test_zero():
-    assert Zero()
-
+def test_how_many_same_letters():
+    assert how_many_same_letters("most", "post") == 3
