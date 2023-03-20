8197b524e1b897bdef4cd870eb0ff71c7f0fc38b
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-20 20:19:20 +0100

    test for more than one max value fails

--- tests/test_zero.py
+++ tests/test_zero.py
@@ -1,6 +1,7 @@
 from kata_closest_to_zero_2.zero import *
 
 list_of_str_for_test = ["post", "most", "frost", "does", "blast", "window"]
+list_of_str_for_test2 = ["postal", "post", "most", "frost", "does", "blast", "window"]
 
 
 def test_how_many_same_letters():
@@ -14,8 +15,14 @@ def test_does_not_count_twice():
 def test_create_list_of_counts():
     assert create_list_of_counts(list_of_str_for_test, "stop") == [4, 3, 3, 2, 2, 1]
 
+
 def test_pick_the_highest_scored_words():
     assert pick_the_highest_scored_word(list_of_str_for_test, "stop") == "post"
+
+
+def test_more_than_one_max_value():
+    assert pick_the_highest_scored_word(list_of_str_for_test2, "stop") == "post"
+
 
 #def test_find_the_shortest():
  #   assert find_the_shortest() ==
