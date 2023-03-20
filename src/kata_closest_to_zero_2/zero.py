d213fbad9e880f5f9f41f308a37c9e3efeb837b6
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-20 19:54:47 +0100

    create list of counts function passed

--- src/kata_closest_to_zero_2/zero.py
+++ src/kata_closest_to_zero_2/zero.py
@@ -5,8 +5,11 @@ def how_many_same_letters(word1, word2):
             count += 1
     return count
 
-def pick_the_highest_same_letters(list_of_str, given_word):
+
+def create_list_of_counts(list_of_str, given_word):
     list_of_counts = []
-    list_of_highest = []
     for word in list_of_str:
-        list_of_counts = how_many_same_letters(word, given_word)
+        list_of_counts.append(how_many_same_letters(word, given_word))
+    return list_of_counts
+
+
