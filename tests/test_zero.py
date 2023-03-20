929bfc986c439b1af816d39b247c9b90a3d3723c
KristynaSodomkova <sodomkova.kristyna@gmail.com>
2023-03-20 20:56:45 +0100

    modified test for highest value indexes

--- tests/test_zero.py
+++ tests/test_zero.py
@@ -20,7 +20,7 @@ def test_find_max_value():
     assert find_max_value(list_of_str_for_test, "stop")
 
 def test_create_list_of_indexes_with_highest_scores():
-    assert create_list_of_indexes_with_highest_scores(list_of_str_for_test2, "stop") == [0 .6]
+    assert create_list_of_indexes_with_highest_scores(list_of_str_for_test2, "stop") == [0,1]
 
 def test_pick_the_highest_scored_words():
     assert pick_the_highest_scored_word(list_of_str_for_test, "stop") == "post"
